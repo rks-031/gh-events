959581d93c3319439c09294090864134f249ea10
Rajnish Kumar Singh <rajnish31july@gmail.com>
2025-02-10 11:34:00 +0530

    skipping the workflow [skip ci]

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -2,6 +2,8 @@ import { string } from 'prop-types';
 
 import './HelpBox.css';
 
+// this is just a demo comment
+
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
